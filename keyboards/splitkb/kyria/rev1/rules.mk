23cf72f945b202bfd5fd3d7f1507ab5e31984ad7
Roberto Santalla <roberto@santalla.io>
2023-09-02 20:16:45 +0200

    kyria/rules.mk: enable CAPS_WORD

--- keyboards/splitkb/kyria/rev1/rules.mk
+++ keyboards/splitkb/kyria/rev1/rules.mk
@@ -13,3 +13,5 @@ OLED_ENABLE = yes          # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
+
+CAPS_WORD_ENABLE = yes
