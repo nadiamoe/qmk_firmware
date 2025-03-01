66f0fbf2a365a60c23a4a6dfe0b914b005529dc6
Nadia Santalla <nadia@santalla.io>
2025-03-01 11:19:05 +0100

    disable rgblight

--- keyboards/splitkb/kyria/rev1/rules.mk
+++ keyboards/splitkb/kyria/rev1/rules.mk
@@ -13,7 +13,7 @@ AUDIO_ENABLE = no          # Audio output
 SWAP_HANDS_ENABLE = yes    # Hand swap magic key
 OLED_ENABLE = yes          # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
 
 CAPS_WORD_ENABLE = yes
